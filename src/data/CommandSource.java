d07bf71b91574477330beb0fd21ce237801b7204
BppleMan <1286363484@qq.com>
2016-06-03 02:22:19 +0800

    一些小的修改

--- src/data/CommandSource.java
+++ src/data/CommandSource.java
@@ -9,12 +9,14 @@ package data;
  */
 public final class CommandSource
 {
+	// 客户端指令
 	public static final String loginCommand = "login";
 	public static final String getFriendCommand = "getfriend";
 	public static final String registCommand = "regist";
 	public static final String byeCommand = "bye";
 	public static final String getFriendInfoCommand = "getfriendinfo";
 
+	// 服务器返回指令
 	public static final String canLoginCommand = "CanLogin";
 	public static final String notCanLoginCommand = "NotCanLogin";
 	public static final String canRegistCommand = "CanRegist";
